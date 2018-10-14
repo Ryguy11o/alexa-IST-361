a8d34d6d806ad7a7b0fce79978386fc8e8cc7509
Ryan O'Neill <rbo5046@psu.edu>
2018-10-14 13:54:01 -0400

    Ask SDK v2 up and working correctly

--- lambda-new.js
+++ lambda-new.js
@@ -47,7 +47,7 @@ const GetNewFactIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'GetNewFactIntent';
   },
   handle(handlerInput) {
-    const slotId = handlerInput.requestEnvelope.request.intent.slots.desciption.resolutions.resolutionsPerAuthority[0].values[0].value.id
+    const slotId = handlerInput.requestEnvelope.request.intent.slots.description.resolutions.resolutionsPerAuthority[0].values[0].value.id
     const speechText = DESCRIPTION_DATE[slotId];
 
     return handlerInput.responseBuilder
