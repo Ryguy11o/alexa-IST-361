7bd51664e0abe5509e865ad92103e94ed42510c4
nql5161 <34181603+nql5161@users.noreply.github.com>
2018-10-21 17:15:28 -0400

    Changed alexa's response

--- handlers/BusStopTimeFinderIntentHandler.js
+++ handlers/BusStopTimeFinderIntentHandler.js
@@ -12,17 +12,18 @@ const BusStopTimeFinderIntentHandler = {
     },
 
     async handle(handlerInput) {
-      // const stopId = handlerInput.requestEnvelope.request.intent.slots.STOP_ID.resolutions.resolutionsPerAuthority[0].values[0].value.id
-      const stopId = 1;
+
+      //sets slot values from AWS Alexa to stopId
+      const stopId = handlerInput.requestEnvelope.request.intent.slots.STOP_ID.resolutions.resolutionsPerAuthority[0].values[0].value.id
       const slotId = handlerInput.requestEnvelope.request.intent.slots.BUS_ROUTE.resolutions.resolutionsPerAuthority[0].values[0].value.id
       const stopInfo = STOP_ID_TO_NAME[stopId];
       const busInfo = BUS_ID_TO_NAME[slotId];
       const nextDeparture = await axios.get(`https://realtime.catabus.com/InfoPoint/rest/StopDepartures/Get/${stopId}`).then(response => response.data);
       const numberOfDepartures = nextDeparture[0].RouteDirections[0].Departures.length;
-
+      //what alexa will say when a successful request is made
       let speechText;
       if(numBuses !== 0){
-          speechText = `The next departure for ${stopInfo.name} is ${nextDeparture}.`;
+          speechText = `There are ${numberOfDepartures} for the ${busInfo.name} at ${stopInfo.name}.`;
       } else {
           speechText = `There are currently no departures listed for the bus stop at ${stopInfo.name}.`;
       }
