20c6d4832f5236fc03cd6db31898e9f529e02461
Ryan O'Neill <rbo5046@psu.edu>
2018-10-21 21:12:58 -0400

    Added main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const Alexa = require('ask-sdk-core');
+const { GetNewFactIntentHandler } = require('./handlers/GetNewFactIntentHandler');
+const { BusFinderIntentHandler } = require('./handlers/BusFinderIntentHandler');
+
+const {
+  LaunchRequestHandler,
+  HelpIntentHandler,
+  CancelAndStopIntentHandler,
+  SessionEndedRequestHandler,
+  ErrorHandler
+} = require('./handlers/BaseHandlers');
+
+exports.handler = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    GetNewFactIntentHandler,
+    BusFinderIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
