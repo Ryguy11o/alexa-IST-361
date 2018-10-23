7f92961d5415c8586118af4ffbfde62973c0135c
nql5161 <34181603+nql5161@users.noreply.github.com>
2018-10-23 18:12:17 -0400

    edited a few lines

--- handlers/BusStopTimeFinderIntentHandler.js
+++ handlers/BusStopTimeFinderIntentHandler.js
@@ -1,18 +1,25 @@
 const axios = require('axios');
 
+console.log('hey');
+
 const {
   STOP_ID_TO_NAME,
   BUS_ID_TO_NAME,
 } = require('../utilities/constants/constants');
 
+console.log('hi');
+
 const BusStopTimeFinderIntentHandler = {
     canHandle(handlerInput) {
       return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'BusStopTimeFinderIntent'
+        && handlerInput.requestEnvelope.request.intent.name === 'BusStopTimeFinderIntent';
     },
+
+console.log('yo');
 
     async handle(handlerInput) {
 
+      console.log('hello');
       //sets slot values from AWS Alexa to stopId
       const stopId = handlerInput.requestEnvelope.request.intent.slots.STOP_ID.resolutions.resolutionsPerAuthority[0].values[0].value.id
       // const slotId = handlerInput.requestEnvelope.request.intent.slots.BUS_ROUTE.resolutions.resolutionsPerAuthority[0].values[0].value.id
