9b0ad717083c27782ecbef038ae147f17c6d3713
Bryce Williams <brycemwilliams25@gmail.com>
2018-10-23 18:38:30 -0400

    SemsterIntent V.4

--- handlers/SemesterIntentHandler.js
+++ handlers/SemesterIntentHandler.js
@@ -1,20 +1,19 @@
 const { SEMESTER_YEAR, SKILL_NAME } = require('../utilities/constants/constants');
 
-const SemesterIntentHandler ={
+const SemesterIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
           && handlerInput.requestEnvelope.request.intent.name === 'SEMESTER_YEAR';
   },
 
   handle(handlerInput) {
-const speechText = SEMESTER_YEAR;
+    const speechText = SEMESTER_YEAR;
 
-return handlerInput.responseBuilder
-.speak(speechText)
-.withSimpleCard(SKILL_NAME, speechText)
-.getResponse();
-
-}
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
+      .getResponse();
+  }
 };
 module.exports = {
   SemesterIntentHandler
