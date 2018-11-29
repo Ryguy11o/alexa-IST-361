6201939e0c1384ca06d162001ba1827b8dbab53a
Nik Lecce <nql5161@psu.edu>
2018-11-29 16:38:37 -0500

    Added basic backbone to this handler

--- handlers/DiningHallMenuIntentHandler.js
+++ handlers/DiningHallMenuIntentHandler.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
 const {
-  STOP_ID_TO_NAME,
-  BUS_ID_TO_NAME,
-  SKILL_NAME
+  SKILL_NAME,
+  LOCATION_ID_TO_NAME,
+  MEAL_TYPE
 } = require('../utilities/constants/constants');
 
 const DiningHallMenuIntentHandler = {
@@ -12,7 +12,22 @@ const DiningHallMenuIntentHandler = {
         && handlerInput.requestEnvelope.request.intent.name === 'DiningHallMenuIntent';
   },
 
-  async handle(handlerInput){
+  async handle(handlerInput) {
+      const menuUrl = `http://menu.hfs.psu.edu/shortmenu.aspx?sName=Penn+State+Housing+and+Food+Services&locationNum=$`;
+      const mealType = handlerInput.requestEnvelope.request.intent.slots.MEAL_TYPE.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+      const diningHall = handlerInput.requestEnvelope.request.intent.slots.LOCATION_NUM.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+      const mealTypeSlot = MEAL_TYPE[mealType];
+      const diningHallSlot = LOCATION_ID_TO_NAME[diningHall];
+      const getMenu = await axios.get(`${menuUrl}${diningHallLocationNum.locationNum}&locationName=${diningHallLocationNum.string}&naFlag=1`)
+      console.log('success');
+      const listOfFoodItems = document.querySelectorAll("#menuDisplay > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div");
 
+  let speechText;
+
+  speechText = `No food available.`;
+  return handlerInput.responseBuilder
+    .speak(speechText)
+    .withSimpleCard(SKILL_NAME, speechText)
+    .getResponse();
   }
 };
