026c8e677aa630e391550e01423f14cf60884dac
KonnorSidler <36972046+KonnorSidler@users.noreply.github.com>
2018-11-30 15:53:20 -0500

    Changed BJCEventIntentHandler to use cheerio
    Changed to Cheerio due to a lighter package for uploading the AWS Lambda.  Supports the next event at the BJC

--- handlers/BJCEventIntentHandler.js
+++ handlers/BJCEventIntentHandler.js
@@ -2,6 +2,29 @@ const { SKILL_NAME } = require('../utilities/constants/constants');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+async function getNextEvent() {
+  let event;
+
+  const options = {
+    uri: 'https://bjc.psu.edu/events-list',
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+
+  rp(options)
+    .then(function ($) {
+      event = $('div.views-row-1').find('div.views-field-title').find('a').text();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
+  await rp(options);
+
+  return event;
+}
+
 const BJCEventIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -10,39 +33,12 @@ const BJCEventIntentHandler = {
 
   async handle(handlerInput) {
     let slotId = null;
-    // if (handlerInput.requestEnvelope.request.intent.slots.EVENT_TYPE.resolutions) {
-    //   slotId = handlerInput.requestEnvelope.request.intent.slots.EVENT_TYPE.resolutions.resolutionsPerAuthority[0].values[0].value.id;
-    //   eventType = EVENT_TYPE_TO_NAME[slotId];
-    // }
 
     let speechText;
-    let event;
-    // let eventTitle = await request-promise;
-    const options = {
-      uri: `https://bjc.psu.edu/events-list`,
-      transform: function (body) {
-        return cheerio.load(body);
-      }
-    };
-
-
-    rp(options)
-      .then(function ($) {
-
-        event = $('div.views-row-1').find('div.views-field-title').find('a').text();
-
-        return event;
-        console.log(title);
-      })
-      .catch(function (err) {
-
-      });
-
-    await rp(options);
-
+    let eventTitle = await getNextEvent();
 
     if (slotId === null) {
-      speechText = `The next event at the BJC is ${event}`;
+      speechText = `The next event at the BJC is ${eventTitle}`;
     } else {
       speechText = 'Something isn\'t working right';
     }
