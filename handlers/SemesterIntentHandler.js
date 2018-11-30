9654afc0c956d5166e2ff7ccd1351e6ba4980691
Nik Lecce <nql5161@psu.edu>
2018-11-29 23:34:48 -0500

    added the fallYear & springYear variables for easy maintainability

--- handlers/SemesterIntentHandler.js
+++ handlers/SemesterIntentHandler.js
@@ -10,22 +10,24 @@ const SemesterIntentHandler = {
     const semesterId = handlerInput.requestEnvelope.request.intent.slots.SEMESTER.resolutions.resolutionsPerAuthority[0].values[0].value.id;
     const academicDescriptions = handlerInput.requestEnvelope.request.intent.slots.ACADEMIC_DESCRIPTION.resolutions.resolutionsPerAuthority[0].values[0].value.id;
     const wordsSaidToAlexa = handlerInput.requestEnvelope.request.intent.slots.ACADEMIC_DESCRIPTION.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+    const fallYear = 2018;
+    const springYear = 2019;
 
     let map;
-    if(semesterId === 'fall_2018'){
+    if(semesterId === `fall_${fallYear}`){
       map = FALL_SCHEDULE;
-    } else if (semesterId === 'spring_2019') {
+    } else if (semesterId === `spring_${springYear}`) {
       map = SPRING_SCHEDULE;
-    } else if (semesterId === 'maymester_2019'){
+    } else if (semesterId === `maymester_${springYear}`){
       map = MAYMESTER;
-    } else if (semesterId === 'summer1_2019'){
+    } else if (semesterId === `summer1_${springYear}`){
       map = SUMMER_SCHEDULE_FIRSTSECTION;
-    } else if (semesterId === 'summer2_2019'){
+    } else if (semesterId === `summer2_${springYear}`){
       map = SUMMER_SCHEDULE_SECONDSECTION;
     }
-    
+
     let speechText;
-    speechText = `The ${wordsSaidToAlexa} is ${map[academicDescriptions]}`;
+    speechText = `${map[academicDescriptions]}`;
     return handlerInput.responseBuilder
       .speak(speechText)
       .withSimpleCard(SKILL_NAME, speechText)
